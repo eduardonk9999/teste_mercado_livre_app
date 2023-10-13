b009fc2be84ad9f2c43d8eb145d639b9f6a6055e
eduardonk9999 <thiago.novak7@gmail.com>
2023-10-13 16:14:12 -0300

    feat: aplicado context no provider e search

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 
 import './Products.scss';
 import fetchProducts from '../../server/products';
 import ProductCard from '../ProductCard/ProductCard';
+import AppContext from '../../context/Context';
 
 function Products() {
-  const [products, setProducts] = useState([]);
+  
+  const {products, setProducts } = useContext(AppContext);
 
   useEffect(() => {
     fetchProducts('iphone').then((response) => {
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import './Search.scss';
 import ButtonSearch from '../ButtonSearch/ButtonSearch';
+import AppContext from '../../context/Context';
+import fetchProducts from '../../server/products';
 
 
 function Search() {
   const [searchValue, setSearchValue] = useState('');
 
+  const { setProducts } = useContext(AppContext);
+
+  const heandleSearchProduct = async (event) => {
+    event.preventDefault();
+
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setSearchValue('');
+  };
+
 
   return(
-    <form className="search">
+    <form className="search" onSubmit={heandleSearchProduct}>
       <input 
         type="text"
         value={searchValue} 
