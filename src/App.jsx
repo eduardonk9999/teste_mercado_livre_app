698af0eefb40aecd8bcb34b7b46437a54db6ac5f
eduardonk9999 <thiago.novak7@gmail.com>
2023-10-13 15:45:06 -0300

    feat: mudando formato

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Products from './components/Products/Products';
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Products />
     </>
   );
 }
